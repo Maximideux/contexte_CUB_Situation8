54d515bbd13badeaab77f7c824f61b36c888afa2
unknown <administrateur@local.edimbourg.cub.sioplc.fr>
2024-03-13 09:43:18 +0100

    Version 1 du script

---
+++ exercice 1-2.ps1
@@ -0,0 +1,8 @@
+﻿#===============================#
+#                               #
+# NOM : scriptProcessusWindows  #
+# AUTEUR : TERPREAU Maximilien  #
+# DATE : 13 mars 2024 à 11h15   #
+#                               #
+#===============================#
+
