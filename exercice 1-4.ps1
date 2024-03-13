f9ac8e887228a522c0e4505965cabe8a6a0e487e
unknown <administrateur@local.edimbourg.cub.sioplc.fr>
2024-03-13 11:03:46 +0100

    Version while

--- exercice 1-4.ps1
+++ exercice 1-4.ps1
@@ -6,8 +6,18 @@
 #                           	#
 #===============================#
 
-[string]$paramètre = Read-Host 'Quel processus désireriez-vous voir?'
+<# Script permettat d'afficher le processus à la demande de l'utilisateur #>
+
+[string]$processus = Read-Host 'Quel processus désirez-vous voir?'
 
 Do {
+    If ($réponse -eq 'Y') {
+        [string]$processus = Read-Host 'Lequel?'
+        } 
+    $tableau = Get-Process | Where-Object {$_.ProcessName -like "$processus"}
+    $tableau | Format-Table Id, Name, Description
     [string]$réponse = Read-Host 'Voulez-vous voir un autre processus? (Y ou n)'
+
 } While ($réponse -eq 'Y')
+
+Write-Host 'Au revoir!'
