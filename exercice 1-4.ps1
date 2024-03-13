9ac508bc9adf8a825d0f0a7a6e901dc33dbeb94b
unknown <administrateur@local.edimbourg.cub.sioplc.fr>
2024-03-13 10:45:37 +0100

    Version 1

---
+++ exercice 1-4.ps1
@@ -0,0 +1,13 @@
+﻿#===============================#
+#                           	#
+# NOM : scriptProsessusWindows  #
+# AUTEUR : TERPREAU Maximilien  #
+# DATE : 13 mars 2024 à 11h15   #
+#                           	#
+#===============================#
+
+[string]$paramètre = Read-Host 'Quel processus désireriez-vous voir?'
+
+Do {
+    [string]$réponse = Read-Host 'Voulez-vous voir un autre processus? (Y ou n)'
+} While ($réponse -eq 'Y')
