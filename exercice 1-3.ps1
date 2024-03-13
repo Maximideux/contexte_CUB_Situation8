d31c94a45fec57db1481cb2241e0a78e6df3f819
unknown <administrateur@local.edimbourg.cub.sioplc.fr>
2024-03-13 10:28:55 +0100

    Version 1

---
+++ exercice 1-3.ps1
@@ -0,0 +1,13 @@
+﻿#===============================#
+#                           	#
+# NOM : scriptProsessusWindows  #
+# AUTEUR : TERPREAU Maximilien  #
+# DATE : 13 mars 2024 à 11h15   #
+#                           	#
+#===============================#
+
+<# Script permettant d'afficher les tables Nom, Id et description #>
+
+$processus = Get-Process | Where-Object {$_.ProcessName -like "svchost"}
+$processus | Format-Table Id, Name, Description
+
