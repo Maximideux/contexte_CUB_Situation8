1622bfc434cc9f136bfa2a8cf2bea6ad9b8992f8
Maximideux <145548182+Maximideux@users.noreply.github.com>
2024-03-20 11:01:59 +0100

    Create exercice 1-9.ps1

---
+++ exercice 1-9.ps1
@@ -0,0 +1,15 @@
+
+
+Import-Csv -Path "C:\service.csv" | ForEach-Object {
+    $serviceName = $_.DisplayName
+    $serviceStatus = $_.Status
+
+    # Vérification si le nom du service contient le mot "system"
+    if ($serviceName -like "*proxy*") {
+        # Affichage du nom du service en vert s'il contient le mot "proxy"
+        Write-Host "$serviceName ($serviceStatus)" -ForegroundColor Green
+    } else {
+        # Affichage du nom du service sans mise en forme spéciale
+        Write-Host "$serviceName ($serviceStatus)"
+    }
+}
