133cffd61cc600c075228142c955b6d4a7e680bd
Maximideux <145548182+Maximideux@users.noreply.github.com>
2024-03-20 11:01:06 +0100

    Update exercice 1-8.ps1

--- exercice 1-8.ps1
+++ exercice 1-8.ps1
@@ -1,3 +1,10 @@
+﻿#========================================#
+#                           	         #
+# NOM : scriptProsessusWindows(Fichiers) #
+# AUTEUR : TERPREAU Maximilien           #
+# DATE : 20 mars 2024 à 11h15            #
+#                           	         #
+#========================================#
 
 function Get-ServicesByStatus {
     param ($status)
