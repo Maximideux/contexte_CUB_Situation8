1b8fffa6cdb60ae2e716c9be755fa99f26418551
Maximideux <145548182+Maximideux@users.noreply.github.com>
2024-03-20 11:02:12 +0100

    Update exercice 1-9.ps1

--- exercice 1-9.ps1
+++ exercice 1-9.ps1
@@ -1,4 +1,10 @@
-
+﻿#===============================#
+#                               #
+# NOM : scriptProcessusWindows  #
+# AUTEUR : TERPREAU Maximilien  #
+# DATE : 13 mars 2024 à 11h15   #
+#                               #
+#===============================#
 
 Import-Csv -Path "C:\service.csv" | ForEach-Object {
     $serviceName = $_.DisplayName
