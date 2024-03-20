68b7dd622afb213eb54d7ec20b806e7553086668
unknown <administrateur@local.edimbourg.cub.sioplc.fr>
2024-03-20 08:48:43 +0100

    Version 1 : Fonction If bolléen fait

---
+++ exercice 1-6.ps1
@@ -0,0 +1,23 @@
+﻿#========================================#
+#                           	         #
+# NOM : scriptProsessusWindows(Fichiers) #
+# AUTEUR : TERPREAU Maximilien           #
+# DATE : 20 mars 2024 à 11h15            #
+#                           	         #
+#========================================#
+
+<# Permet de rentrer les résulats dans un fichier .txt et .csv #>
+
+[bool]$existe = Test-Path "C:\sauvegardeSituation8_CUB\"
+
+if ($existe -eq $false)
+{
+    Write-Host "Le dossier n'existe pas. Je vais le créer." 
+    New-Item "C:\sauvegardeSituation8_CUB\" -itemType Directory
+    Write-Host "Dossier créer, on passe à la suite" 
+    
+    }else{
+
+    Write-Host "Le dossier existe déjà, on passe à la suite."
+}
+
