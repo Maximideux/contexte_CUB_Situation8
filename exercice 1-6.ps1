2550818d00952b4d7977d2b3599b6d96071f5f17
unknown <administrateur@local.edimbourg.cub.sioplc.fr>
2024-03-20 09:31:30 +0100

    Version finale

--- exercice 1-6.ps1
+++ exercice 1-6.ps1
@@ -8,16 +8,44 @@
 
 <# Permet de rentrer les résulats dans un fichier .txt et .csv #>
 
-[bool]$existe = Test-Path "C:\sauvegardeSituation8_CUB\"
+[bool]$existeDossier = Test-Path "C:\sauvegardeSituation8_CUB\"
+[bool]$existeFichierTxt =  Test-Path "C:\sauvegardeSituation8_CUB\sauvegarde.txt"
+[bool]$existeFichierCsv =  Test-Path "C:\sauvegardeSituation8_CUB\sauvegarde.csv"
 
 if ($existe -eq $false)
 {
-    Write-Host "Le dossier n'existe pas. Je vais le créer." 
     New-Item "C:\sauvegardeSituation8_CUB\" -itemType Directory
-    Write-Host "Dossier créer, on passe à la suite" 
     
     }else{
 
     Write-Host "Le dossier existe déjà, on passe à la suite."
+
+    if ($existeFichierTXT -eq $true)
+    {
+        Write-Host "Le fichier sauvegarde.txt existe déjà."
+        }else{ 
+        New-Item "C:\sauvegardeSituation8_CUB\sauvegarde.txt" -itemType File
+    }
+
+    if ($existeFichierCSV -eq $true)
+    {
+        Write-Host "Le fichier sauvegarde.csv existe déjà."
+        }else{
+        New-Item "C:\sauvegardeSituation8_CUB\sauvegarde.csv" -itemType File
+        }
 }
 
+[string]$processus = Read-Host 'Quel processus désirez-vous voir?'
+
+Do {
+    If ($réponse -eq 'Y') {
+        [string]$processus = Read-Host 'Lequel ?'
+        } 
+    $tableau = Get-Process | Where-Object {$_.ProcessName -like "$processus"}
+    $tableau | Format-Table Id, Name, Description | Out-File "C:\sauvegardeSituation8_CUB\sauvegarde.txt" -Encoding utf8
+    $tableau | Export-Csv "C:\sauvegardeSituation8_CUB\sauvegarde.csv" -Delimiter ","  
+    [string]$réponse = Read-Host 'Voulez-vous voir un autre processus? (Y ou n)'
+
+} While ($réponse -eq 'Y')
+
+Write-Host 'Au revoir!'
