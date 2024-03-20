855abb23bb65275182088c1aa139478b37412c09
Maximideux <145548182+Maximideux@users.noreply.github.com>
2024-03-20 11:00:44 +0100

    Create exercice 1-8.ps1

---
+++ exercice 1-8.ps1
@@ -0,0 +1,17 @@
+
+function Get-ServicesByStatus {
+    param ($status)
+
+    Write-Host "Liste des services $status :"
+    $services = Get-Service | Where-Object { $_.Status -eq $status }
+    $services | Select-Object DisplayName, Description, Status
+}
+
+# Demande à l'utilisateur de saisir l'état des services qu'il souhaite afficher
+$status = Read-Host "Entrez l'état des services à afficher (Running ou Stopped)"
+
+# Appel de la fonction Get-ServicesByStatus pour obtenir les services selon l'état spécifié par l'utilisateur
+$services = Get-ServicesByStatus -status $status
+
+# Exportation des informations dans un fichier CSV
+$services | Export-Csv -Path "C:/service_$status.csv" -NoTypeInformation -Encoding utf8
